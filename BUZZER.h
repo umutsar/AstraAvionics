3a1aa1a89c72eb5626375fecc05d7e4ad0861513
Umut Şar <contact@umutsar.com>
2025-08-08 04:00:30 +0300

    Update BUZZER.h

--- BUZZER.h
+++ BUZZER.h
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #ifndef BUZZER_H
 #define BUZZER_H
 
@@ -7,11 +8,14 @@ class Buzzer {
 private:
   int buzzerPin;
   bool state;
+  uint32_t timer1;
+  bool first;
 
 public:
   Buzzer() {
     buzzerPin = 5;
     state = false;
+    first = 1;
   }
 
   void begin(int pin = 5) {
@@ -49,6 +53,20 @@ public:
 
   bool isOn() {
     return state;
+  }
+
+  void controlSound() {
+    if((millis() - timer1 > 10000 || first) && !state) {
+      on(); 
+      first = 0;
+      state = true;
+      timer1 = millis();
+    }
+    else {
+      off();
+      state = false;
+    }
+
   }
 };
 
